8df8fc10eb6ea65a3fe576f38e92f99a495ce1fd
SharyZ <onshariiit@gmail.com>
2022-04-08 21:00:31 +0500

    create category page with dynamic slug

---
+++ components/PageTitle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <h2 class="title">{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default() {
+        return "";
+      },
+    },
+  },
+  computed: {
+    normalizedTitles() {
+      return this.title.trim().toLowerCase();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  @apply border-l-4 border-primary-500 px-4;
+}
+.title {
+  @apply text-3xl capitalize text-slate-50;
+}
+</style>
---
+++ pages/category/_slug.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <PageTitle :title="slug" />
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: ["auth"],
+  async asyncData({ params }) {
+    const slug = params.slug;
+    return { slug };
+  },
+};
+</script>
